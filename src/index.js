6fca502c1129b231b860b0b0223d248a1dc5d314
Sean F Quinn <sean@esqew.com>
2022-07-18 11:32:17 -0400

    converted jpg to webp

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ import './style.scss';
  */
 (async () => {
     const imgNum = Math.floor(Math.random() * NUMBER_OF_PICS) + 1;
-    document.querySelector("#bottom img#cover").setAttribute('src', `./img/${imgNum}.jpg`);
+    document.querySelector("#bottom img#cover").setAttribute('src', `./img/${imgNum}.webp`);
 })();
