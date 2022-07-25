867fc880b15ab615ddc5c6113f42c5a50007dc7d
Sean F Quinn <sean@esqew.com>
2022-07-25 10:45:43 -0400

    added image info + logic to add titles for images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const NUMBER_OF_PICS = 7;
 
 import './style.scss';
+import imageInfo from './img/images.json'
 
 /**
  * Set random image for display
  */
 (async () => {
     const imgNum = Math.floor(Math.random() * NUMBER_OF_PICS) + 1;
-    document.querySelector("#bottom img#cover").setAttribute('src', `./img/${imgNum}.webp`);
+    const imageElement = document.querySelector("#bottom img#cover");
+    imageElement.setAttribute('src', `./img/${imgNum}.webp`);
+    imageElement.setAttribute('title', `${imageInfo[imgNum - 1].title}\n\n© ${imageInfo[imgNum - 1].year} ${imageInfo[imgNum - 1].author}, All rights reserved.`);
 })();
