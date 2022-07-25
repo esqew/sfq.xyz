ba523c1a9490bb7313c07d5bbd5e04747a3d6e99
Sean F Quinn <sean@esqew.com>
2022-07-25 14:22:36 -0400

    refactored random image selection and metadata; removed unnececssary jpg originals

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-const NUMBER_OF_PICS = 7;
-
 import './style.scss';
-import imageInfo from './img/images.json'
+import imageData from './img/images.json'
 
 /**
  * Set random image for display
  */
 (async () => {
-    const imgNum = Math.floor(Math.random() * NUMBER_OF_PICS) + 1;
+    const selectedImage = imageData[Math.floor(Math.random() * imageData.length)];
     const imageElement = document.querySelector("#bottom img#cover");
-    imageElement.setAttribute('src', `./img/${imgNum}.webp`);
-    imageElement.setAttribute('title', `${imageInfo[imgNum - 1].title}\n\n© ${imageInfo[imgNum - 1].year} ${imageInfo[imgNum - 1].author}, All rights reserved.`);
+    imageElement.setAttribute('src', `./img/${selectedImage.file}`);
+    imageElement.setAttribute('title', `${selectedImage.title}\n\n© ${selectedImage.year} ${selectedImage.author}, All rights reserved.`);
 })();
